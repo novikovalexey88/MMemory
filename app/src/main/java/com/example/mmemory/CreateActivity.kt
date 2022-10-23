610da62a67a45e5fabbb3d666bacfd009e944513
novikovalexey88 <novikov.a.m@yandex.ru>
2022-10-23 23:32:02 +0300

    Build RecyclerView + Adapter for Creation Custom Image

--- app/src/main/java/com/example/mmemory/CreateActivity.kt
+++ app/src/main/java/com/example/mmemory/CreateActivity.kt
@@ -1,24 +1,43 @@
 package com.example.mmemory
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mmemory.models.BoardSize
 import com.example.mmemory.utils.EXTRA_BOARD_SIZE
+import java.net.URI
 
 class CreateActivity : AppCompatActivity() {
 
+    private  lateinit var rvImagePicker: RecyclerView
+    private  lateinit var etGameName: EditText
+    private  lateinit var btnSave: Button
+
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        rvImagePicker = findViewById(R.id.rvimagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btnSave)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Выберите картинок (0/ $numImagesRequired)"
+
+        rvImagePicker.adapter = ImagePickerrAdapter(this, chosenImageUris, boardSize)
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
